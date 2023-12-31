bc79c42485b260974ed55986511a0693bc88bc82
Durga Sainath Dora <durgasainathdora@gmail.com>
2023-12-31 18:16:31 +0530

    customized the home page

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,21 @@
+//next images
+import Image from "next/image";
+
+//next link
+import Link from "next/link";
+
+//icons
+import { HiArrowRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0">
+      <Link href={"/work"} className="relative w-[185px] h-[185px] flex justify-center items-center group">
+        <Image src={'/rounded-text.png'} width={141} height={148} alt="" className="animate-spin-slow w-full h-full max-w-[141px] max-h-[140px]"/>
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all-0.3 duration-300"/>
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
