b2521070d3f3ff54a021c5b13479312b0b6bd8df
Durga Sainath Dora <durgasainathdora@gmail.com>
2024-01-28 10:20:49 +0530

    added title

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ethan-portfolio-starter
+# Durga Sainath PortFolio
--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
+      <title>PortFolio</title>
       <AnimatePresence mode="wait">
         <motion.div key={router.route} className="h-full">
           <Transition />
